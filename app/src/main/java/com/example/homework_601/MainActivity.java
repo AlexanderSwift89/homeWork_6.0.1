caf4e46d50219270b32ffed75b7c7bb6b71cd0c8
AlexanderSwift89 <alexander.lyapunov89@gmail.com>
2020-08-29 19:14:10 +0300

    The commit of 2020-08-29

---
+++ app/src/main/java/com/example/homework_601/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.homework_601;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView displayNum;
+    private Button btn0, btn1,btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnDot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+        //нажатие на кнопку "0"
+        btn0.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                displayNum.setText(0);
+            }
+        });
+        //нажатиен на кнопку "1"
+        btn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                displayNum.setText(1);
+            }
+        });
+
+        //нажатиен на кнопку "2"
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                displayNum.setText(2);
+            }
+        });
+
+        //нажатиен на кнопку "3"
+        btn3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                displayNum.setText(3);
+            }
+        });
+
+    }
+
+    private void initViews(){
+        displayNum = findViewById(R.id.displayNum);
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+        btnDot = findViewById(R.id.btnDot);
+    }
+}
